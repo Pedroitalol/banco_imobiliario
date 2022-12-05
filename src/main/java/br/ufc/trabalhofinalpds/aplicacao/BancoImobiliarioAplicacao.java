6134d10f50657c09fbe73e6664bb08944d7e91e0
joaoeliaslv <ljoaoelias0@gmail.com>
2022-12-05 13:54:47 -0300

    Redimensionamento para tamanho padrão do tabuleiro e casas, criação e inserção de arte das casas sorte/reves, prisão e ponto de partida.

--- src/main/java/br/ufc/trabalhofinalpds/aplicacao/BancoImobiliarioAplicacao.java
+++ src/main/java/br/ufc/trabalhofinalpds/aplicacao/BancoImobiliarioAplicacao.java
@@ -11,7 +11,7 @@ public class BancoImobiliarioAplicacao extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(BancoImobiliarioAplicacao.class.getResource("bancoimobiliario.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Scene scene = new Scene(fxmlLoader.load(), 752.84, 768);
         stage.setTitle("Banco Imobilariadasso");
         stage.setScene(scene);
         stage.show();
