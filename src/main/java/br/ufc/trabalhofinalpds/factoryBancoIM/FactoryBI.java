f173cbe08ecfb6a741d319eb743d66b0a47cadc4
WalessonSG <walesson.sg@gmail.com>
2022-12-08 23:42:15 -0300

    init factory, esqueleto base.

---
+++ src/main/java/br/ufc/trabalhofinalpds/factoryBancoIM/FactoryBI.java
@@ -0,0 +1,13 @@
+package br.ufc.trabalhofinalpds.factoryBancoIM;
+
+public class FactoryBI {
+
+    public static IBancoIMtype createBI(int type){
+        if(type == 1){
+            return null;
+        }
+        else {
+            return null;
+        }
+    }
+}
---
+++ src/main/java/br/ufc/trabalhofinalpds/factoryBancoIM/IBancoIMtype.java
@@ -0,0 +1,6 @@
+package br.ufc.trabalhofinalpds.factoryBancoIM;
+
+public interface IBancoIMtype {
+    //falta o tabuleiro
+    public String RIP();
+}
