b1db9a74e1bf2bfd3c7dbb2dfd0a981198f2840a
Pedro Ítalo Lobo <pedro@ongsys.com.br>
2022-12-09 16:10:35 -0300

    Mudanças na fabrica do tabuleiro

--- src/main/java/br/ufc/trabalhofinalpds/Tabuleiro/TabuleiroPadrao.java
+++ src/main/java/br/ufc/trabalhofinalpds/Tabuleiro/TabuleiroPadrao.java
@@ -1,10 +1,30 @@
 package br.ufc.trabalhofinalpds.Tabuleiro;
 
+import java.util.ArrayList;
+
+import br.ufc.trabalhofinalpds.casa.*;
+import br.ufc.trabalhofinalpds.dado.DadoController;
 import br.ufc.trabalhofinalpds.factoryBancoIM.ITabuleiroType;
 
 public class TabuleiroPadrao implements ITabuleiroType {
     //para futuras implementações de novos tipos de tabuleiros.
-    @Override
+    private ArrayList<ICasa> casas;
+    private DadoController dado;
+
+    public TabuleiroPadrao(DadoController dado){
+        //aqui cria as casas:
+        this.casas = new ArrayList<ICasa>();
+
+        casas.add(new CasaPropriedade("Leblon", 100, 6));
+        casas.add(new CasaSorte());
+        casas.add(new CasaPropriedade("Av. Presidente Vargas", 60, 2));
+        casas.add(new CasaPropriedade("Av. Nossa Sr. de Copacabana", 60, 4));
+        casas.add(new CasaCompanhia(dado, 150));
+        casas.add(new CasaSorte());
+        casas.add(new CasaPropriedade("Av. Nossa Sr. de Copacabana", 60, 4));
+        
+    }
+
     public int umTabuleiroQualquer() {
         return 1;
     }
